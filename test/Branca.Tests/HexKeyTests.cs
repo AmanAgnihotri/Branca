d0e0f62d775c8e23dc029a3c17cd3fad2b2627c9
AmanAgnihotri <aman.agnihotri@yahoo.com>
2021-08-20 09:35:53 +0530

    Add HexKeyTests

---
+++ test/Branca.Tests/HexKeyTests.cs
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright © 2021 Aman Agnihotri
+
+namespace Branca.Tests
+{
+  using System;
+  using Xunit;
+  using static TestData;
+
+  public sealed class HexKeyTests
+  {
+    [Theory(DisplayName = "Valid hex keys get parsed successfully.")]
+    [InlineData(ValidKey01)]
+    [InlineData(ValidKey02)]
+    public void ValidHexKeysGetParsedSuccessfully(string value)
+    {
+      HexKey key = value;
+
+      Assert.True(key.Bytes.Length == 32);
+    }
+
+    [Theory(DisplayName = "Invalid hex keys throw ArgumentException.")]
+    [InlineData(InvalidKey01)] // Short key
+    [InlineData(InvalidKey02)] // Contains non-hexadecimal characters
+    public void InvalidHexKeysThrowArgumentException(string value)
+    {
+      Assert.Throws<ArgumentException>(() =>
+      {
+        HexKey _ = value;
+      });
+    }
+  }
+}
