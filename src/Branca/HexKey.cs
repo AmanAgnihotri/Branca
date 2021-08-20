e14a0de2074289c78042118e4b91c4f445d2d436
AmanAgnihotri <aman.agnihotri@yahoo.com>
2021-08-20 09:06:34 +0530

    Add HexKey
    HexKey represents an abstraction for Branca secret key. HexKey is one of the ways in which Branca can be provided this secret key.

---
+++ src/Branca/HexKey.cs
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright © 2021 Aman Agnihotri
+
+namespace Branca
+{
+  using System;
+
+  public sealed record HexKey
+  {
+    private const int KeyLength = 32;
+    private const int HexKeyLength = KeyLength * 2;
+
+    public ReadOnlyMemory<byte> Bytes { get; }
+
+    public HexKey(string hexKey)
+    {
+      if (hexKey is null || hexKey.Length != HexKeyLength)
+      {
+        string message = $"Key must have exactly {HexKeyLength} characters.";
+
+        throw new ArgumentException(message, nameof(hexKey));
+      }
+
+      Bytes = hexKey.AsBytesFromHexString();
+    }
+
+    public static implicit operator HexKey(string hexKey)
+    {
+      return new HexKey(hexKey);
+    }
+  }
+}
