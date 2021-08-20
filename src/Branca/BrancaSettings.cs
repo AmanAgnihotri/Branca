a801e2d34b948110f3e4f182c4d681ed3ae0f40f
AmanAgnihotri <aman.agnihotri@yahoo.com>
2021-08-20 09:03:03 +0530

    Add BrancaSettings
    - MaxStackLimit denotes the maximum allocation in bytes that will happen on stack at any given time.
    - TokenLifeTimeInSeconds denotes the lifetime of the Branca token.
    - Timer is for managing the Branca timestamp.

---
+++ src/Branca/BrancaSettings.cs
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright © 2021 Aman Agnihotri
+
+namespace Branca
+{
+  public sealed record BrancaSettings
+  {
+    public uint MaxStackLimit { get; init; } = 1024;
+
+    public uint? TokenLifetimeInSeconds { get; init; } = 3600;
+
+    public ITimer Timer { get; init; } = new Timer();
+  }
+}
