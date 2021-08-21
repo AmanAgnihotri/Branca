d0d63371988dacd9854479ef15f006abd91929d1
AmanAgnihotri <aman.agnihotri@yahoo.com>
2021-08-21 16:57:49 +0530

    Add BrancaState

---
+++ test/Branca.Tests/BrancaState.cs
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright © 2021 Aman Agnihotri
+
+namespace Branca.Tests
+{
+  using System;
+
+  public sealed record BrancaState
+  {
+    public HexKey Key { get; }
+
+    public ReadOnlyMemory<byte> Nonce { get; }
+
+    public uint Timestamp { get; }
+
+    public string Token { get; }
+
+    public ReadOnlyMemory<byte> Message { get; }
+
+    public bool IsValid { get; }
+
+    public BrancaState(
+      HexKey key,
+      string? nonce,
+      uint timestamp,
+      string token,
+      string message,
+      bool isValid)
+    {
+      Key = key;
+      Nonce = nonce.AsBytesFromHexString();
+      Timestamp = timestamp;
+      Token = token;
+      Message = message.AsBytesFromHexString();
+      IsValid = isValid;
+    }
+  }
+}
