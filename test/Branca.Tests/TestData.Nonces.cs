8acf068def3696c706dfa24d918c06bd55ef3055
AmanAgnihotri <aman.agnihotri@yahoo.com>
2021-08-21 16:58:14 +0530

    Add TestData.Nonces

---
+++ test/Branca.Tests/TestData.Nonces.cs
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright © 2021 Aman Agnihotri
+
+namespace Branca.Tests
+{
+  public static partial class TestData
+  {
+    public const string Nonce =
+      "beefbeefbeefbeefbeefbeefbeefbeefbeefbeefbeefbeef";
+
+    public const string? NullNonce = null;
+  }
+}
