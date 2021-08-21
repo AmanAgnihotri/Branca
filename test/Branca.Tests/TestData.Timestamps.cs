01da318d55054605db40074b3c45269941fb38d8
AmanAgnihotri <aman.agnihotri@yahoo.com>
2021-08-21 16:58:20 +0530

    Add TestData.Timestamps

---
+++ test/Branca.Tests/TestData.Timestamps.cs
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright © 2021 Aman Agnihotri
+
+namespace Branca.Tests
+{
+  public static partial class TestData
+  {
+    public const uint MinTime = 0;
+
+    public const uint MaxTime = 4294967295;
+
+    public const uint Nov2773 = 123206400;
+  }
+}
