223bc2b8c1f36880c0764bb2200361d51b2ba6f0
AmanAgnihotri <aman.agnihotri@yahoo.com>
2021-08-21 16:58:07 +0530

    Add TestData.Messages

---
+++ test/Branca.Tests/TestData.Messages.cs
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright © 2021 Aman Agnihotri
+
+namespace Branca.Tests
+{
+  public static partial class TestData
+  {
+    public const string EmptyMessage = "";
+
+    public const string Hex80Message = "80";
+
+    public const string Null8Message = "0000000000000000";
+
+    public const string HelloMessage = "48656c6c6f20776f726c6421";
+  }
+}
