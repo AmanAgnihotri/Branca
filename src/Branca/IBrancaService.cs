93fd0f6b2715eb143d8ebb6646f4f199f7d5c218
AmanAgnihotri <aman.agnihotri@yahoo.com>
2022-04-15 11:56:33 +0530

    Add IBrancaService

---
+++ src/Branca/IBrancaService.cs
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright © 2022 Aman Agnihotri
+
+namespace Branca;
+
+using System;
+
+public interface IBrancaService
+{
+  string Encode(string payload);
+
+  string Encode(string payload, uint createTime);
+
+  string Encode(ReadOnlySpan<byte> payload);
+
+  string Encode(ReadOnlySpan<byte> payload, uint createTime);
+
+  bool TryDecode(string token, out byte[] payload);
+
+  bool TryDecode(string token, out byte[] payload, out uint createTime);
+}
